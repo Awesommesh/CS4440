72a64cfcc7cc9286b04579d53b9b4431e912bc75
Riya Agrawal <43094144+ragrawal4545@users.noreply.github.com>
2020-04-14 20:20:21 -0400

    human readable -> wnid, and imagenet parent/child request implemented

--- syn-search.py
+++ syn-search.py
@@ -1,36 +1,35 @@
 import nltk 
-from nltk.corpus import wordnet 
-
-query = input("Enter your search query: ")
-
-#searching from wordnet 
-syn = wordnet.synsets(query) 
-print(nltk.word_tokenize(syn[0].definition()))
+import requests
+from nltk.corpus import wordnet
+import re 
 
 words = []
-#searching from synset.txt
-synset = open("synset.txt","r", 1)
+
+#human readable query -> wnid
+query = input("Enter your search query: ")
+#searching from words.txt
+synset = open("words.txt","r", 1)
+synset.seek(0)
 for line in synset:
-    stripped_line = line.strip()
-    if (query.lower() in stripped_line.lower()):
-        words += stripped_line.split(",")
-        break
+    if (query.lower() in line.lower()):
+        wnid = line[0:9]
+        words.append(wnid)
 synset.close()
+
+
+#get children wnid
+payload = {'full': '1', 'wnid': words[0]}
+r = requests.get('http://www.image-net.org/api/text/wordnet.structure.hyponym', params = payload)
+# print(r.url)
+page_contents = r.text #string
+# print (type(page_contents))
+page_contents_to_list = re.split(r'[\n\r]\s*', page_contents)
+words.append(page_contents_to_list[0]) #html page formatted weird
+for id in page_contents_to_list[1:]:
+    words.append(id[1:10])
+del words[-1] #again, weird '' at end of list
+
+
 print (words)
 
-#rough
-# synset = open("synset.txt","r", 1)
-# x = synset.read().find(query) #gives starting index, without newlines
 
-# synset.seek(0)
-# c = synset.read().count('\n', 0, x) #counting newlines
-
-# synset.seek(x+c-10, 0)  
-# line = synset.readline(100)
-# words = line.split(",")   
-# # words = nltk.word_tokenize(line)
-# print (words)
-
-# synset.close()
-
-# print("For: ", query) 
