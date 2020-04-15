f895954a9647e6e661605811a50fee7dca574ea3
Riya Agrawal <43094144+ragrawal4545@users.noreply.github.com>
2020-04-14 20:37:49 -0400

    wnid grabber - complete

--- syn-search.py
+++ syn-search.py
@@ -3,33 +3,43 @@ import requests
 from nltk.corpus import wordnet
 import re 
 
-words = []
+wnid = []
+local_wnid = []
+children_wnid = []
 
-#human readable query -> wnid
-query = input("Enter your search query: ")
-#searching from words.txt
-synset = open("words.txt","r", 1)
-synset.seek(0)
-for line in synset:
-    if (query.lower() in line.lower()):
-        wnid = line[0:9]
-        words.append(wnid)
-synset.close()
+def get_local_wnid(query):
+    #human readable query -> wnid
+    synset = open("words.txt","r", 1) #searching from words.txt
+    synset.seek(0)
+    for line in synset:
+        if (query.lower() in line.lower()):
+            id = line[0:9]
+            local_wnid.append(id)
+    synset.close()
+
+def get_children_wnid(wnid):
+    #get children wnid
+    payload = {'full': '1', 'wnid': wnid}
+    r = requests.get('http://www.image-net.org/api/text/wordnet.structure.hyponym', params = payload)
+    page_contents = r.text #string
+    page_contents_to_list = re.split(r'[\n\r]\s*', page_contents)
+    #append to list
+    children_wnid.append(page_contents_to_list[0]) #html page formatted weird
+    for id in page_contents_to_list[1:]:
+        children_wnid.append(id[1:10])
+    del children_wnid[-1] #again, weird '' at end of list
+
+def main():
+     query = input("Enter your search query: ")
+     get_local_wnid(query)
+     for id in local_wnid: 
+         get_children_wnid(id)
+     wnid = local_wnid + children_wnid
+     print (wnid)
+
+if __name__ == "__main__":
+    main()
 
 
-#get children wnid
-payload = {'full': '1', 'wnid': words[0]}
-r = requests.get('http://www.image-net.org/api/text/wordnet.structure.hyponym', params = payload)
-# print(r.url)
-page_contents = r.text #string
-# print (type(page_contents))
-page_contents_to_list = re.split(r'[\n\r]\s*', page_contents)
-words.append(page_contents_to_list[0]) #html page formatted weird
-for id in page_contents_to_list[1:]:
-    words.append(id[1:10])
-del words[-1] #again, weird '' at end of list
-
-
-print (words)
 
 
