34e995ce18f39ca12c709f8a573484f7e0b3b141
Ubuntu <ubuntu@ip-172-31-47-8.us-east-2.compute.internal>
2020-04-21 18:00:26 +0000

    minor fixes

--- app/service/wnid_searcher.py
+++ app/service/wnid_searcher.py
@@ -4,28 +4,15 @@ from nltk.corpus import wordnet
 import re
 import random
 
-<<<<<<< HEAD
-=======
 wnid = set()
 local_wnid = set()
 children_wnid = set()
 
->>>>>>> 7afb4b4a8960ee4e473b0dae8bf90627422537c4
 def toString(list):
     string = "\n".join(list)
     return string
 
 def get_wnid(query):
-<<<<<<< HEAD
-     wnid = []
-     local_wnid = []
-     children_wnid = []
-     get_local_wnid(query, local_wnid)
-     for id in local_wnid: 
-         get_children_wnid(id, children_wnid)
-     wnid = local_wnid + children_wnid
-     return toString(wnid)
-=======
      local_wnid.clear()
      children_wnid.clear()
      get_local_wnid(query)
@@ -34,9 +21,8 @@ def get_wnid(query):
      wnid = local_wnid | children_wnid
      wnid = clean_wnid(wnid)
      return toString(list(filter(None,(list(wnid)))))
->>>>>>> 7afb4b4a8960ee4e473b0dae8bf90627422537c4
 
-def get_local_wnid(query, local_wnid):
+def get_local_wnid(query):
     #human readable query -> wnid
     synset = open('words.txt',"r", 1)
     synset.seek(0)
@@ -50,7 +36,7 @@ def get_local_wnid(query, local_wnid):
             local_wnid.add(id)
     synset.close()
 
-def get_children_wnid(wnid, children_wnid):
+def get_children_wnid(wnid):
     #get children wnid
     payload = {'full': '1', 'wnid': wnid}
     r = requests.get('http://www.image-net.org/api/text/wordnet.structure.hyponym', params = payload)
