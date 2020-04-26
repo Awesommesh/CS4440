2371530566702a9cd9fbf9726b973e90498012b7
Animesh Agrawal <animesh.a.777@gmail.com>
2020-04-26 12:48:16 +0800

    removed aws credential and put placeholder

--- upload.js
+++ upload.js
@@ -1,81 +1,4 @@
 // Remember to include jQuery somewhere.
-/*
-$(function() {
-
-  $('#theForm').on('submit', sendFile);
-});
-
-var s3 = new AWS.S3({
-  region: 'us-east-2',
-  signatureVersion: 'v4',
-  accessKeyId: 'AKIA2LNZHUGZJL2DTE53',
-  secretAccessKey: '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'
-});
-
-var imagenetApiEndpoint = "http://imagenet-nlb-b4fcd380459e9b61.elb.us-east-2.amazonaws.com";
-
-var metadata = {};
-function sendFile(e) {
-    //document.write(9);
-    // get the reference to the actual file in the input
-    e.preventDefault();
-
-    var theFormFile = $('#theFile').get()[0].files[0];
-    $.ajax({
-        url: imagenetApiEndpoint + '/metadata?query_class=' + $('input[name="uploadClass"]').val(),
-        type: 'GET',
-        success: function(data) {
-            console.log($('input[name="uploadClass"]').val());
-            // returnedImage = '';
-            // returnedImage = '<div>';
-            metadata = JSON.parse(JSON.stringify(data));
-            console.log(metadata.class);
-            console.log(metadata.index);
-            if (metadata !== {}) {
-                document.getElementById("query_input").value = metadata.class;
-                $("#textform").submit()
-            } else {
-                alert('Enter an imagenet class')
-            }
-        }
-
-    });
-    var uploadPreSignedUrl = s3.getSignedUrl('putObject', {
-        Bucket: 'imagenetbucket',
-        Key: 'test_in/test.jpg',
-        ACL: 'authenticated-read',
-        // This must match with your ajax contentType parameter
-        ContentType: 'binary/octet-stream',
-        Metadata: metadata
-        /* then add all the rest of your parameters to AWS puttObect here */
-    /*}, function (err, url) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log('The URL is', url);
-        }
-    });
-    console.log(uploadPreSignedUrl);
-    console.log('test_in/test.jpg');
-    console.log(s3);
-    $.ajax({
-        type: 'PUT',
-        url: uploadPreSignedUrl,
-        // Content type must much with the parameter you signed your URL with
-        contentType: 'binary/octet-stream',
-        // this flag is important, if not set, it will try to send data as a form
-        processData: false,
-        // the actual file is sent raw
-        data: theFormFile,
-        success: function() {
-            console.log("yas");
-        }
-    });
-    return false;
-  }
-*/
-// Remember to include jQuery somewhere.
-
 $(function() {
 
   $('#theForm').on('submit', sendFile);
@@ -137,8 +60,8 @@ function sendFile(e) {
 var s3 = new AWS.S3({
   region: 'us-east-2',
   signatureVersion: 'v4',
-  accessKeyId: 'AKIA2LNZHUGZJL2DTE53',
-  secretAccessKey: '0fLjbbK0P2cqJnXAP6TwUHRt6za6zQeQSBkJpuzU'
+  accessKeyId: 'REPLACE ME WITH YOUR ACCESS KEY ID',
+  secretAccessKey: 'REPLACE ME WITH YOUR SECRET ACCESS KEY'
 });
 
 var uploadPreSignedUrl = s3.getSignedUrl('putObject', {
